4821b8f15bd36d374758fdf0d1f7da4f34f76721
AndrewFernando <72968022@continental.edu.pe>
2024-03-31 11:43:34 -0500

    ECommerce Lightbox

---
+++ js/app.js
@@ -0,0 +1,40 @@
+
+/*
+Hacemos click en una imagen pequena 
+    lightbox aparece y anadimos una clase 'activo'
+    recogemos la imagen pequena 
+    a la imagen grande colocas scr de la imagen pequena 
+*/
+
+
+const enlaces = document.querySelectorAll('.ul .a')
+const lightbox = document.querySelector('.lightbox')
+const grande = document.querySelector('.grande')
+const cerrar = document.querySelector('.cerrar')
+const buttons = document.querySelector('.buttons')
+const botones = document.querySelectorAll('.boton');
+
+enlaces.forEach((cadaEnlace,i)=>{   
+    enlaces[i].addEventListener('click',(e)=>{
+        e.preventDefault()
+        let ruta = cadaEnlace.querySelector('.img').src
+        console.log(ruta)
+        lightbox.classList.add('activo')
+        grande.setAttribute('src',ruta)
+        buttons.style.display = 'none';
+    })
+})
+console.log(cerrar);
+cerrar.addEventListener('click',(e)=>{
+    console.log("Clic en el botón de cerrar");
+    lightbox.classList.remove('activo')
+    grande.setAttribute('src','')
+    buttons.style.display = 'flex';
+})
+
+buttons.addEventListener('click', (event) => {
+    const buttonIndex = [...buttons.children].indexOf(event.target);
+    if (buttonIndex !== -1) {
+        window.location.href = `index${buttonIndex + 1}.html`;
+    }
+});
